7d4aff356647265c5e4f1da65e7d1ef2e122e765
PAT <56907263+ganggyunggyu@users.noreply.github.com>
2023-07-11 16:09:21 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,23 +1,23 @@
 function solution(n, computers) {
-    const visited = Array.from({length : n}, ()=>false)
-    console.log(visited)
+    const visited = Array.from({length : 3}, ()=>false)
+
     let cnt = 0
-    
-    const dfs = (i) => {
-        visited[i] = true
+
+    const dfs = (v) => {
+        visited[v] = true
         
-        for(let j = 0; j < computers[i].length; j++){
-            if(computers[i][j] === 1 && !visited[j]){
-                dfs(j)
-            }
+        for(let j=0; j<computers[v].length; j++){
+            if(computers[v][j] == 1 && !visited[j])
+            dfs(j)
         }
     }
     
-    for(let i=0; i<computers.length; i++){
+    for(let i=0; i<n; i++){
         if(!visited[i]){
-        dfs(i)
-        cnt++   
+            dfs(i)
+            cnt++
         }
     }
     return cnt
+
 }
